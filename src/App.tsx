1106dbed80402e48f20468527fcc7e695b72e647
alesso-chiavarino <alessochiavarino@gmail.com>
2023-10-05 01:09:21 -0300

    prueba 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,62 +4,75 @@ import React, { useState } from 'react';
 
 const App = () => {
 
-  // type NodeName = {
-  //   [index: number]: string
-  // }
-
-  const [nodes, setNodes] = useState([]);
-  const [nodeName, setNodeName] = useState('')
-  // const [nodeNames, setNodeNames] = useState<NodeName>({});
-  const [isShowForm, setisShowForm] = useState(true)
-
-  const handleNodes = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNodeName(e.target.value);
+  type NodeName = {
+    [index: number]: string
   }
 
-  const handleShowForm = () => {
-    setisShowForm(!isShowForm);
+  const [numNodes, setNumNodes] = useState(0);
+  const [nodeNames, setNodeNames] = useState<NodeName>({});
+
+  const adjacencyMatrix = [
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ];
+
+  const handleNumNodes = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumNodes(e.target.valueAsNumber);
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setNodes([...nodes, { name: nodeName }]);
-    setNodeName('');
+  const handleNodes = (e: React.ChangeEvent<HTMLInputElement>, nodeIndex: number) => {
+    setNodeNames({
+      ...nodeNames,
+      [nodeIndex]: e.target.value
+    });
+  }
+
+  const mapNodesForm = () => {
+    const nodesForm = [];
+    for (let i = 0; i < numNodes; i++) {
+      nodesForm.push(
+        <div key={i}>
+          <hr />
+          <div className='flex flex-col gap-2'>
+            <label htmlFor={`node${i}`}>Nombre del nodo {i + 1}</label>
+            <input onChange={(e) => handleNodes(e, i)} className='text-black' type="text" name={`node${i}`} id={`node${i}`} />
+          </div>
+        </div>
+      );
+    }
+    return nodesForm;
   }
 
   // Construir la cadena DOT a partir de la matriz de adyacencia
   let dotString = 'digraph {';
-  for (let i = 0; i < nodes.length; i++) {
-    dotString += `${nodes[i].name};`;
+  for (let i = 0; i < adjacencyMatrix.length; i++) {
+    for (let j = 0; j < adjacencyMatrix[i].length; j++) {
+      if (adjacencyMatrix[i][j] === 1) {
+        const sourceNodeName = nodeNames[i] || String.fromCharCode(97 + i);
+        const targetNodeName = nodeNames[j] || String.fromCharCode(97 + j);
+        dotString += ` ${sourceNodeName} -> ${targetNodeName};`;
+      }
+    }
   }
   dotString += '}';
 
   return (
-    <div className='flex justify-between'>
-      <div className='w-[20%]'>
-        <button onClick={handleShowForm} className='bg-green-400 px-4 py-2 rounded-md'>Form</button>
-        {isShowForm && (
-          <form className='flex flex-col gap-5' onSubmit={handleSubmit}>
-            <div className='flex flex-col gap-2'>
-              <label htmlFor="numNodes">Nombre del nodo</label>
-              <input onChange={handleNodes} value={nodeName} className='text-black' name="numNodes" id="numNodes" />
-            </div>
-            <button className='bg-red-400 px-4 py-2 rounded-md'>Añadir</button>
-          </form>
-        )}
-        <div>
-          <h5>Nodos</h5>
-          <div>
-            {nodes.length > 0 && nodes.map((node, index) => (
-              <div key={index} className='border-2 rounded-md border-red-400 w-fit'>
-                <span>{node.name}</span>
-              </div>
-            ))} 
+    <div className='flex'>
+      <div className='w-[50%]'>
+        <form className='flex flex-col gap-5'>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="numNodes">Número de nodos</label>
+            <input onChange={handleNumNodes} className='text-black' type="number" name="numNodes" id="numNodes" />
           </div>
-        </div>
+          {mapNodesForm()}
+          <button className='bg-red-400 px-4 py-2 rounded-md'>Dibujar</button>
+        </form>
       </div>
-      <div className='w-[80%]'>
-        {/* <Graphviz dot={dotString} /> */}
+      <div className='w-[50%]'>
+        <Graphviz dot={dotString} />
       </div>
     </div>
   );
